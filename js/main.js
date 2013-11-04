8ff5025d5fa1c0babe43efef67fb6f38bd19d9e0
Bogdan Varticovschi <emptimd@gmail.com>
2013-11-04 12:53:16 +0200

    Adding img folder and few changes.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
 	
+	/**
+	 * Script for back-to-top button effect
+	 */
+	$(window).scroll(function () {
+		if ($(this).scrollTop() != 0) {
+			$('#bttop').fadeIn();
+		} else {
+			$('#bttop').fadeOut();
+		}
+	});
+	$('#bttop').click(function () {
+		$('body,html').animate({scrollTop: 0}, 800);
+	});
 });
