c7cab0b931738e93ff79e3095f3e113cf58f20ea
Bogdan Varticovschi <emptimd@gmail.com>
2013-11-09 10:47:15 +0200

    Fixed version 1.1. Stable version 1.1.1

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+/*=====UNCOMMENT ANY OF THIS FUNCTIONS TO USE THEM INTO MAIN.JS====*/
+
+
+/**
+ * Automaticaly add * for label that contains *:required. 
+ */
+// function label_required() {
+// 	$('*:required').parent('label').append('*');
+// }
+
+
+/**
+ * Fade button + scroll to top on click.
+ */
+// function window_scrool() {
+// 	$(window).scroll(function () {
+// 		if ($(this).scrollTop() != 0) {
+// 			$('#bttop').fadeIn();
+// 		} else {
+// 			$('#bttop').fadeOut();
+// 		}
+// 	});
+// 	$('#bttop').click(function () {
+// 		$('body,html').animate({scrollTop: 0}, 800);
+// 	});
+// }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 jQuery(function($) {
-
+	
 	// label_require();
 	/**
 	 * Script for back-to-top button effect
